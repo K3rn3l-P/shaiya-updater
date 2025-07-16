07aacaa0bbeea02942e3545eb9e403fa80647053
K3rn3l-P <davidcocco@outlook.it>
2025-07-16 22:52:54 +0200

    MOD

--- Updater/Common/Constants.cs
+++ Updater/Common/Constants.cs
@@ -2,7 +2,7 @@
 {
     static class Constants
     {
-        public const int UpdaterVersion = 5;
+        public const int UpdaterVersion = 6;
         public const string Source = "https://duff.pinto-lime.ts.net/";
         public const string WebBrowserSource = "https://duff.pinto-lime.ts.net/shaiya/index.html";
     }
